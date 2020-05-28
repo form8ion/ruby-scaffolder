379e772e61cd4acd1921df5f4a00c872520fcd5d
Matt Travi <programmer@travi.org>
2020-05-28 10:44:41 -0500

    test(integration): removed expected prompt handling code
    since nothing like this is needed yet and this is no longer the modern approach anyway

--- test/integration/features/step_definitions/common-steps.js
+++ test/integration/features/step_definitions/common-steps.js
@@ -2,7 +2,6 @@ import {exists, promises as fsPromises} from 'fs';
 import {promisify} from 'util';
 import {resolve} from 'path';
 import stubbedFs from 'mock-fs';
-// import bddStdIn from 'bdd-stdin';
 import sinon from 'sinon';
 import {assert} from 'chai';
 import any from '@travi/any';
@@ -16,10 +15,6 @@ const {readFile} = fsPromises;
 const existsAsync = promisify(exists);
 
 Before(async function () {
-  // work around for overly aggressive mock-fs, see:
-  // https://github.com/tschaub/mock-fs/issues/213#issuecomment-347002795
-  require('mock-stdin'); // eslint-disable-line import/no-extraneous-dependencies
-
   stubbedFs({
     templates: {
       'Rakefile.rb': await readFile(resolve(__dirname, '../../../../', 'templates/Rakefile.rb')),
@@ -41,23 +36,7 @@ Given('the default answers are chosen', async function () {
 });
 
 When(/^the project is scaffolded$/, async function () {
-  // const shouldBeScopedAnswer = 'Public' === this.visibility ? ['\n'] : [];
   this.projectName = any.word();
-
-  // bddStdIn(...[
-  //   '\n',
-  //   // ...this.projectTypeAnswer,
-  //   // ...shouldBeScopedAnswer,
-  //   // '\n',
-  //   // '\n',
-  //   // '\n',
-  //   // '\n',
-  //   // ...this.unitTestAnswer,
-  //   // ...this.integrationTestAnswer,
-  //   // ...this.ciAnswer ? this.ciAnswer : [],
-  //   // ...'application' === this.projectType ? this.applicationTypeAnswer : [],
-  //   // ...this.transpilationLintAnswer ? this.transpilationLintAnswer : []
-  // ]);
 
   scaffoldResult = await scaffold({
     projectRoot: process.cwd(),
